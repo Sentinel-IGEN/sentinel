3a24a0542e02b333af49a3e02141cfcca22830d0
Kevin Peng <kevinpkf@gmail.com>
2022-10-06 18:27:09 -0700

    Change base endpoint to health check

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth(): string {
+    return 'App is running.';
   }
 }
