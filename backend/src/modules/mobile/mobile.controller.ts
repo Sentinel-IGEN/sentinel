fda44d5bdaa5a942d41aff56ec7608b9f0d876b1
Kevin Peng <kevinpkf@gmail.com>
2022-11-18 18:17:32 -0800

    Finished register device token endpoint

--- backend/src/modules/mobile/mobile.controller.ts
+++ backend/src/modules/mobile/mobile.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Param, Body, Post, HttpException } from '@nestjs/common';
 import { SNSService, MongoService } from '../../services';
 import { User } from '../../schemas/User.schema';
-import { RegisterMobileDeviceTokenDTO } from '../../schemas/dtos'
-
+import { RegisterMobileDeviceTokenDTO } from '../../schemas/dtos';
+import { NotFoundException } from '@aws-sdk/client-sns';
 @Controller('/mobile')
 export class MobileController {
   constructor(
@@ -15,62 +15,105 @@ export class MobileController {
   async createUser(@Body() user: User): Promise<User | unknown> {
     try {
       const data = await this.MongoService.createUser(user);
-      return data
+      return data;
     } catch (err) {
       return err;
     }
   }
 
   // Register Mobile Device Token: should be able to be called multiple times without failure
+  // See https://docs.aws.amazon.com/sns/latest/dg/mobile-platform-endpoint.html#mobile-platform-endpoint-pseudo-code for breakdown
   @Post('/registerDeviceToken')
   async registerMobileUser(@Body() data: RegisterMobileDeviceTokenDTO) {
     try {
-      if(data.deviceToken && data.type && data.userId) {
+      if (data.deviceToken && data.type && data.userId) {
         // get platformApplicationARN from supplied os type
-        const platformApplicationARN = this.SNSService.getPlatformApplicationARNByOS(data.type)
+        const platformApplicationARN =
+          this.SNSService.getPlatformApplicationARNByOS(data.type);
 
-        if(platformApplicationARN === undefined) {
-          throw new HttpException("Supplied OS type cannot be mapped to a Platform Application", 400)
+        if (platformApplicationARN === undefined) {
+          throw new HttpException(
+            'Supplied OS type cannot be mapped to a Platform Application',
+            400,
+          );
         }
 
         // Get user by id and check if platform endpoint ARN is stored yet
-        const user = await this.MongoService.getUser(data.userId)
+        const user = await this.MongoService.getUser(data.userId);
 
-        if(user === null) {
-          throw new HttpException("Could not find user with supplied userId", 404)
+        if (user === null) {
+          throw new HttpException(
+            'Could not find user with supplied userId',
+            404,
+          );
         }
 
         // If endpoint ARN is not stored, create platform endpoint for user and store it
-        if(user.endpointARN === undefined) {
-          const newEndpointARN = await this.SNSService.createEndpoint(platformApplicationARN, data.deviceToken)
-          await this.MongoService.updateUser(data.userId, {endpointARN: newEndpointARN})
+        let newEndpointARN: string | undefined;
+        if (user.endpointARN === undefined) {
+          console.log('ENDPOINT ARN was not previously stored for the user');
+
+          newEndpointARN = await this.SNSService.createEndpoint(
+            platformApplicationARN,
+            data.deviceToken,
+          );
+          console.log('created platform endpoint using AWS SNS');
+
+          await this.MongoService.updateUser(data.userId, {
+            endpointARN: newEndpointARN,
+          });
+          console.log('updated user in mongoDB with new endpoint arn');
         }
 
-        /**
-         * TODO: finish rest of attribute checks
-         * 
-         * 
-         * Pseudocode:
-         call get endpoint attributes on the platform endpoint ARN 
+        try {
+          const attributes =
+            await this.SNSService.getPlatformEndpointAttributes(
+              user.endpointARN || newEndpointARN,
+            );
+          console.log(`got endpoint attributes: ${attributes}`);
 
-          if (while getting the attributes a not-found exception is thrown)
-            # the platform endpoint was deleted 
-            call create platform endpoint with the latest device token
-            store the returned platform endpoint ARN
-          else 
-            if (the device token in the endpoint does not match the latest one) or 
-                (get endpoint attributes shows the endpoint as disabled)
-              call set endpoint attributes to set the latest device token and then enable the platform endpoint
-            endif
-          endif
-         */
+          if (
+            attributes?.Token !== data.deviceToken ||
+            attributes?.Enabled === 'false'
+          ) {
+            console.log(
+              "either stored token doesn't match supplied token, or endpoint is disabled",
+            );
+            // set endpoint attributes here
+            const metaData =
+              await this.SNSService.setPlatformEndpointAttributes(
+                user.endpointARN || newEndpointARN,
+                {
+                  Token: data.deviceToken,
+                  Enabled: 'true',
+                },
+              );
+            console.log(
+              `set platform attributes with new token and Enabled: true, here is the return: ${metaData}`,
+            );
+          }
+        } catch (err) {
+          console.log(`caught err when getting endpoint attributes: ${err}`);
+          if (err instanceof NotFoundException) {
+            newEndpointARN = await this.SNSService.createEndpoint(
+              platformApplicationARN,
+              data.deviceToken,
+            );
+            console.log(
+              `caught non found error, meaning endpoint was deleted. created new endpoint with ARN: ${newEndpointARN}`,
+            );
 
+            await this.MongoService.updateUser(data.userId, {
+              endpointARN: newEndpointARN,
+            });
+            console.log('updated user in mongoDB with new endpointARN');
+          }
+        }
       } else {
-        throw new HttpException("JSON body is missing required fields", 400)
+        throw new HttpException('JSON body is missing required fields', 400);
       }
     } catch (err) {
       return err;
     }
   }
-
 }
--- backend/src/services/sns/sns.service.ts
+++ backend/src/services/sns/sns.service.ts
@@ -3,6 +3,8 @@ import {
   SNSClient,
   CreateTopicCommand,
   CreatePlatformEndpointCommand,
+  GetEndpointAttributesCommand,
+  SetEndpointAttributesCommand,
 } from '@aws-sdk/client-sns';
 
 @Injectable()
@@ -36,6 +38,41 @@ export class SNSService {
 
     const response = await this.sns.send(command);
     return response.EndpointArn;
+  }
+
+  /*
+  Attributes include the following:
+
+    CustomUserData – arbitrary user data to associate with the endpoint. Amazon SNS does not use this data. The data must be in UTF-8 format and less than 2KB.
+
+    Enabled – flag that enables/disables delivery to the endpoint. Amazon SNS will set this to false when a notification service indicates to Amazon SNS that the endpoint is invalid. Users can set it back to true, typically after updating Token.
+
+    Token – device token, also referred to as a registration id, for an app and mobile device. This is returned from the notification service when an app and mobile device are registered with the notification service.
+
+    The device token for the iOS platform is returned in lowercase.
+  */
+  async getPlatformEndpointAttributes(endpointARN: string | undefined) {
+    const command = new GetEndpointAttributesCommand({
+      EndpointArn: endpointARN,
+    });
+
+    const response = await this.sns.send(command);
+    return response.Attributes;
+  }
+
+  // set platform endpoint attributes
+  // returns metadata
+  async setPlatformEndpointAttributes(
+    endpointARN: string | undefined,
+    attributes: Record<string, string>,
+  ) {
+    const command = new SetEndpointAttributesCommand({
+      EndpointArn: endpointARN,
+      Attributes: attributes,
+    });
+
+    const response = await this.sns.send(command);
+    return response;
   }
 
   getPlatformApplicationARNByOS(os: 'iOS' | 'Android') {
