80a0ef9172968207bf50c8b9deca3daebb9588ae
Kevin Peng <kevinpkf@gmail.com>
2023-03-14 22:28:40 -0700

    added gps caching in mongodb on MQTT event

--- backend/src/schemas/EmbeddedDevice.schema.ts
+++ backend/src/schemas/EmbeddedDevice.schema.ts
@@ -5,6 +5,7 @@ export interface EmbeddedDevice {
   uuid: string; // hardware uuid to uniquely identify device, created on physical device creation
   tokenHash: string; // hashed token for authorizing endpoint consumption by the embedded device
   userId?: Types.ObjectId; // user uuid to relate to User object in DB
+  gpsLog?: string[];
 }
 
 // Object in DB should be created when device is manufactured, so userId will not exist yet
@@ -12,4 +13,5 @@ export const EmbeddedDeviceMongooseSchema = new Schema<EmbeddedDevice>({
   uuid: { type: String, required: true, unqiue: true, index: true },
   tokenHash: { type: String, required: true, unqiue: true },
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  gpsLog: [String],
 });
--- backend/src/schemas/dtos.ts
+++ backend/src/schemas/dtos.ts
@@ -4,12 +4,14 @@ export interface CreateDeviceDTO {
   uuid: string; // hardware uuid to uniquely identify device, created on physical device creation
   token: string; // token for authorizing endpoint consumption by the embedded device
   userId?: Types.ObjectId; // user uuid to relate to User object in DB
+  gpsLog?: string[];
 }
 
 export interface UpdateDeviceDTO {
   uuid: string; // hardware uuid to uniquely identify device, created on physical device creation
   token?: string; // token for authorizing endpoint consumption by the embedded device
   userId?: Types.ObjectId; // user uuid to relate to User object in DB
+  gpsLog?: string[];
 }
 
 export interface RegisterMobileDeviceTokenDTO {
