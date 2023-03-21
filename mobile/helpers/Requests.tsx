0498b2cb253284702f0a1d3e5e9bc277e459e09b
Kevin Peng <kevinpkf@gmail.com>
2023-03-20 19:37:02 -0700

    add request helper

--- mobile/helpers/Requests.tsx
+++ mobile/helpers/Requests.tsx
@@ -1,8 +1,12 @@
 import { API_URL } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const sendPostRequest = async (endpoint: string, body: any): Promise<Response> => {
+const sendPostRequest = async (
+  endpoint: string,
+  body: any
+): Promise<Response> => {
   try {
-    console.log(`POST at: ${API_URL}/${endpoint}`)
+    console.log(`POST at: ${API_URL}/${endpoint}`);
     const res = await fetch(`${API_URL}/${endpoint}`, {
       method: "POST",
       headers: {
@@ -17,4 +21,18 @@ const sendPostRequest = async (endpoint: string, body: any): Promise<Response> =
   }
 };
 
-export { sendPostRequest };
+const getBikeLocationHistory = async (): Promise<
+  Array<{ location: string; time: number }>
+> => {
+  try {
+    const embeddedDeviceId = await AsyncStorage.getItem("@embeddedDeviceId");
+    const res = await fetch(`${API_URL}/logs/${embeddedDeviceId}`);
+
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+export { sendPostRequest, getBikeLocationHistory };
