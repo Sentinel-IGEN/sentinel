007e6cd0ce9fdcd41c170655ca71b644ccd5499b
Kevin Peng <kevinpkf@gmail.com>
2023-04-08 20:26:26 -0700

    DAID: enable cors and add mqtt propogate to mobile endpoint (#21)
    * enable cors
    
    * add endpoint to propogate mqtt to mobile
    
    * change endpoint name to simulateMQTTMessage

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { WsAdapter } from '@nestjs/platform-ws';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
